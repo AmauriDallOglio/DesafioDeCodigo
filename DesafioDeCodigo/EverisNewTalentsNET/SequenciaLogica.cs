13f4b2a69b38eb423a3c45b744f9f3c85e65cf3f
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2024-04-03 19:49:13 -0300

    Desafios

---
+++ DesafioDeCodigo/EverisNewTalentsNET/SequenciaLogica.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDeCodigo.EverisNewTalentsNET
+{
+    public class SequenciaLogica
+    {
+        public void Executar()
+        {
+
+        }
+    }
+}
+
+
+/*
+ * 
+ * 
+ * 
+ * Desafio
+Desenvolva um programa que seja capaz de ler um valor inteiro positivo N. Em seguida, o programa deverá imprimir N * 2 linhas de saída, conforme o padrão especificado abaixo. Para os valores numéricos que possuam mais de seis dígitos, todos os dígitos devem ser exibidos.
+
+Entrada
+O arquivo de entrada contém um único número inteiro positivo N (1 < N < 1000).
+
+Saída
+Para cada número i no intervalo de 1 a N, seu programa deve imprimir duas linhas. A primeira linha deve mostrar o número i, o seu quadrado (i^2), e o seu cubo (i^3). Na segunda linha, o número i deve ser seguido pelo quadrado de i mais 1, e pelo cubo de i mais 1. Siga o formato apresentado nos exemplos de saída.
+
+Exemplo de Entrada	Exemplo de Saída
+5
+
+1 1 1
+1 2 2
+2 4 8
+2 5 9
+3 9 27
+3 10 28
+4 16 64
+4 17 65
+5 25 125
+5 26 126
+
+Observe que a sequência de saída envolve padrões aritméticos progressivos para cada número i, iniciando com o próprio i, seguido pelo cálculo do seu quadrado (i^2) e cubo (i^3). Além de variações desses valores na linha subsequente, onde é impresso i, seguido do seu quadrado + 1 e, por fim, seu cubo + 1.
+
+
+ * 
+ * 
+ * 
+ */
---
+++ DesafioDeCodigo/EverisNewTalentsNET/TempoJogoComMinutos.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDeCodigo.EverisNewTalentsNET
+{
+    public class TempoJogoComMinutos
+    {
+        public void Executar()
+        {
+            Console.WriteLine("Digite o valor:");
+            string[] input = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(input[0]);
+            int minutoInicial = int.Parse(input[1]);
+            int horaFinal = int.Parse(input[2]);
+            int minutoFinal = int.Parse(input[3]);
+
+            int duracaoEmMinutos = 0;
+
+            if (horaInicial == horaFinal && minutoInicial == minutoFinal)
+            {
+                // Caso os horários iniciais e finais sejam iguais, o jogo durou 24 horas
+                duracaoEmMinutos = 24 * 60;
+            }
+            else if (horaInicial == horaFinal)
+            {
+                // Caso as horas iniciais e finais sejam iguais, mas os minutos sejam diferentes
+                duracaoEmMinutos = minutoFinal - minutoInicial;
+            }
+            else
+            {
+                // Calcula a duração do jogo em minutos
+                duracaoEmMinutos = ((horaFinal - horaInicial) * 60) + (minutoFinal - minutoInicial);
+            }
+
+            if (duracaoEmMinutos <= 0)
+            {
+                // Se a duração for negativa ou igual a zero, ajusta para 24 horas
+                duracaoEmMinutos += 24 * 60;
+            }
+
+            int duracaoEmHoras = duracaoEmMinutos / 60;
+            int duracaoRestanteEmMinutos = duracaoEmMinutos % 60;
+
+            Console.WriteLine($"O JOGO DUROU {duracaoEmHoras} HORA(S) E {duracaoRestanteEmMinutos} MINUTO(S)");
+        }
+    
+    }
+}
+
+/*
+ * 
+ * 
+ * Desafio
+Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
+
+Obs: O jogo tem duração mínima de um (1) minuto e duração máxima de 24 horas.
+
+Entrada
+Quatro números inteiros representando a hora de início e fim do jogo.
+
+Saída
+Mostre a seguinte mensagem: “O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)” .
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+7 8 9 10
+
+O JOGO DUROU 2 HORA(S) E 2 MINUTO(S)
+
+7 7 7 7
+
+O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
+
+7 10 8 9
+
+O JOGO DUROU 0 HORA(S) E 59 MINUTO(S)
+ * 
+ * 
+ */
